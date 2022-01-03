fe0daa630e9860d9ddf57ef286d26d2734aa1988
Muhammad Hassan Naeem <nhassan_1990@yahoo.com>
2022-01-03 19:49:57 +0500

    bug fixes

--- src/Store/reducers/comments.js
+++ src/Store/reducers/comments.js
@@ -9,11 +9,7 @@ export default (state = INIT_STATE, action) => {
   switch (action.type) {
     case USER_COMMENTS:
       const commentsClone = state.comments.slice(0);
-      // const temporary_list_for_user_comment = [];
-      // temporary_list_for_user_comment.push(action.payload);
-      // console.log("commentsClone action.payload", action.payload);
       commentsClone.push(action.payload);
-      console.log("commentsClone data added in it", commentsClone);
       return {
         ...state,
         comments: commentsClone,
