70ae8ed3a9ca381db1d1198b86e9349a828678b6
Muhammad Hassan Naeem <nhassan_1990@yahoo.com>
2022-01-12 15:43:37 +0500

    fixes

--- src/Store/actions/commentsstorageaction.js
+++ src/Store/actions/commentsstorageaction.js
@@ -77,4 +77,21 @@ export const addPromos = (promos) => ({
 // .then(response => response.json())
 // .then(dishes => dispatch(addDishes(dishes)))
 // .catch(error => dispatch(dishesFailed(error.message)));
+// return fetch(baseUrl + 'dishes')
+// .then(response => {
+//     if (response.ok) {
+//       return response;
+//     } else {
+//       var error = new Error('Error ' + response.status + ': ' + response.statusText);
+//       error.response = response;
+//       throw error;
+//     }
+//   },
+//   error => {
+//         var errmess = new Error(error.message);
+//         throw errmess;
+//   })
+// .then(response => response.json())
+// .then(dishes => dispatch(addDishes(dishes)))
+// .catch(error => dispatch(dishesFailed(error.message)));
 
