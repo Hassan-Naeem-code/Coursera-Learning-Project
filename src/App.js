630d9608ed41a0dda18aac83b5769fd72e4ff703
Muhammad Hassan Naeem <nhassan_1990@yahoo.com>
2022-01-21 17:13:20 +0500

    fixes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import MainComponent from "./component/MainComponent";
 import { BrowserRouter as Router } from "react-router-dom";
-import {Provider} from 'react-redux';
-import store, {persistor} from './Store/index';
-import {PersistGate} from 'redux-persist/integration/react';
+import { Provider } from "react-redux";
+import store, { persistor } from "./Store/index";
+import { PersistGate } from "redux-persist/integration/react";
 import "./App.css";
 
 function App() {
@@ -20,3 +20,19 @@ function App() {
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <Provider store={store}>
+//       <PersistGate persistor={persistor}>
+//         <Router>
+//           <div className="App">
+//             <MainComponent />
+//           </div>
+//         </Router>
+//       </PersistGate>
+//     </Provider>
+//   );
+// }
+
+// export default App;
